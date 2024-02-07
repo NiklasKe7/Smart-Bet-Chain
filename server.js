68b775a3abe5bc4d6278c42ffc0a0d2404ddff0d
NiklasKe7 <n-kemmer@t-online.de>
2024-02-07 14:59:49 +0100

    Added Adminboard

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/index.html"));
 })
-
+app.get("/adminboard", (req, res) => {
+    res.sendFile(path.join(__dirname + "/adminboard.html"));
+})
 const server = app.listen(5000);
 const portNumber = server.address().port;
 console.log(`port is open on ${portNumber}`);
